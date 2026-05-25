fb816056ebd63b5d6785a86571c9c69e5de5df24
Zbigniew Sienkiewicz <zbs.sienkiewicz@gmail.com>
2026-05-25 22:06:46 +0200

    Add precomputed move tables for all piece types, create get_random_move function, optimize engine.

--- src/hexengine.h
+++ src/hexengine.h
@@ -7,9 +7,11 @@
 
 #include <string>
 #include <vector>
+#include <cstdint>
 
 namespace hexengine {
     constexpr int HEXBOARD_SIZE = 91;
+    constexpr int QRS_SIZE = 3;
     constexpr int MAX_KING_MOVES = 12;
     constexpr int MAX_KNIGHT_MOVES = 12;
     constexpr int ROOK_RAYS = 6;
@@ -42,38 +44,32 @@ namespace hexengine {
         int s;
     };
 
-    using Board = Piece[HEXBOARD_SIZE];
+    using BoardArray = Piece[HEXBOARD_SIZE];
     using Color = PieceTurn;
 
     struct Move {
-        int from;
-        int to;
-        Piece captured_piece;
-        PieceType promotion;
+        int from{};
+        int to{};
+        Piece captured_piece{};
+        PieceType promotion = PieceType::Empty;
+        bool is_en_passant = false;
+        int prev_en_passant_square = -1;
     };
 
     struct HBoard {
-        Board board;
+        BoardArray board_array;
         PieceTurn turn;
+        int en_passant_square = -1;
+        int king_pos[2] = {-1, -1};
     };
 
-    const HBoard &get_main_board();
-
-    extern int king_moves[HEXBOARD_SIZE][MAX_KING_MOVES];
-    extern int knight_moves[HEXBOARD_SIZE][MAX_KNIGHT_MOVES];
-    extern int rook_moves[HEXBOARD_SIZE][ROOK_RAYS][MAX_ROOK_RAY_LENGTH];
-    extern int bishop_moves[HEXBOARD_SIZE][BISHOP_RAYS][MAX_BISHOP_RAY_LENGTH];
-    extern int pawn_moves[HEXBOARD_SIZE][PAWN_COLOR][PAWN_MOVE_TYPE][MAX_PAWN_MOVE_TYPE_MOVES];
-
-    /**
-     * Initializes the `hex_qrs` table with coordinates for a hexagonal grid.
-     *
-     */
-    void init_hex_qrs_table();
+    HBoard &get_main_board();
 
     void init_hexengine();
 
     void setup_board(HBoard &board);
+
+    void setup_startup_board(HBoard &board);
 
     const CubeCoords &get_hex_qrs(int index);
 
@@ -87,50 +83,69 @@ namespace hexengine {
 
     void set_turn(PieceTurn turn);
 
-    void init_king_moves();
-
-    void init_knight_moves();
-
-    void init_rook_moves();
-
-    void init_bishop_moves();
-
-    void init_pawn_moves();
-
     std::string move_to_uci(const Move &move);
 
-    bool is_checked(const HBoard &board, const Move &move);
+    bool is_checked(HBoard &board, const Move &move);
 
     bool is_king_in_check(const HBoard &board, PieceSide side);
 
-    bool make_move(const Move &move);
+    void get_legal_king_moves(HBoard &board, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_king_moves(const HBoard &board, bool only_captures = false);
+    void get_legal_king_moves_at(HBoard &board, int index, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_king_moves_at(const HBoard &board, int index, bool only_captures = false);
+    void get_legal_knight_moves(HBoard &board, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_knight_moves(const HBoard &board, bool only_captures = false);
+    void get_legal_knight_moves_at(HBoard &board, int index, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_knight_moves_at(const HBoard &board, int index, bool only_captures = false);
+    void get_legal_rook_moves(HBoard &board, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_rook_moves(const HBoard &board, bool only_captures = false);
+    void get_legal_rook_moves_at(HBoard &board, int index, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_rook_moves_at(const HBoard &board, int index, bool only_captures = false);
+    void get_legal_bishop_moves(HBoard &board, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_bishop_moves(const HBoard &board, bool only_captures = false);
+    void get_legal_bishop_moves_at(HBoard &board, int index, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_bishop_moves_at(const HBoard &board, int index, bool only_captures = false);
+    void get_legal_queen_moves(HBoard &board, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_queen_moves(const HBoard &board, bool only_captures = false);
+    void get_legal_queen_moves_at(HBoard &board, int index, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_queen_moves_at(const HBoard &board, int index, bool only_captures = false);
+    void get_legal_pawn_moves(HBoard &board, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_pawn_moves(const HBoard &board, bool only_captures = false);
+    void get_legal_pawn_moves_at(HBoard &board, int index, std::vector<Move> &moves, bool only_captures = false);
 
-    std::vector<Move> get_legal_pawn_moves_at(const HBoard &board, int index, bool only_captures = false);
+    void get_legal_moves_at(HBoard &board, int index, std::vector<Move> &moves);
 
-    std::vector<Move> get_legal_moves_at(const HBoard &board, int index);
+    std::vector<Move> get_all_legal_moves(HBoard &board, bool only_captures = false);
 
-    std::vector<Move> get_all_legal_moves(const HBoard &board, bool only_captures = false);
+    Move get_random_move(HBoard &board);
+
+    void make_move(HBoard &board, const Move &move);
+
+    void unmake_move(HBoard &board, const Move &move);
+
+    struct PerftResults {
+        std::uint64_t nodes = 0;
+        std::uint64_t captures = 0;
+        std::uint64_t en_passants = 0;
+        std::uint64_t promotions = 0;
+        std::uint64_t promotion_captures = 0;
+        std::uint64_t checks = 0;
+        std::uint64_t checkmates = 0;
+        std::uint64_t stalemates = 0;
+    };
+
+    std::uint64_t perft(HBoard &board, int depth);
+
+    PerftResults perfts(HBoard &board, int depth);
+
+    void perftd(HBoard &board, int depth);
+
+    std::string smart_int_print(std::uint64_t val);
+
+    extern const int king_moves[HEXBOARD_SIZE][MAX_KING_MOVES];
+    extern const int knight_moves[HEXBOARD_SIZE][MAX_KNIGHT_MOVES];
+    extern const int rook_moves[HEXBOARD_SIZE][ROOK_RAYS][MAX_ROOK_RAY_LENGTH];
+    extern const int bishop_moves[HEXBOARD_SIZE][BISHOP_RAYS][MAX_BISHOP_RAY_LENGTH];
+    extern const int pawn_moves[HEXBOARD_SIZE][PAWN_COLOR][PAWN_MOVE_TYPE][MAX_PAWN_MOVE_TYPE_MOVES];
 }
 #endif //GLAUCUS_HEXENGINE_H
